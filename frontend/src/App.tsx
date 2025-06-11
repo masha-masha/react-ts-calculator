b2f02f1a2b3d0758aa6a38d80a633140f67bea32
Maria Bazhenova <yankeevsdixie@gmail.com>
2025-06-11 16:13:16 +0300

    add calculator with css without js

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -3,7 +3,33 @@ import './App.css'
 function App() {
 
   return (
-    <h1>Calculator</h1>
+    <>
+    <h1 className='calculator-title'>Let's count!</h1>
+    <div className="calculator">
+    <div className="display" id="display">0</div>
+    <div className="buttons">
+      <button className="number">7</button>
+      <button className="number">8</button>
+      <button className="number">9</button>
+      <button className="operator">/</button>
+
+      <button className="number">4</button>
+      <button className="number">5</button>
+      <button className="number">6</button>
+      <button className="operator">x</button>
+
+      <button className="number">1</button>
+      <button className="number">2</button>
+      <button className="number">3</button>
+      <button className="operator">+</button>
+
+      <button className="number">0</button>
+      <button className="number">.</button>
+      <button className="equals">=</button>
+      <button className="operator">-</button>
+    </div>
+  </div>
+    </>
   )
 }
 
